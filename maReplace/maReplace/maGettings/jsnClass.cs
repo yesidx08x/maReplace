158d74ceb34a03952f8a7cb4202b573de096be55
Axe King <yes_id@163.com>
2020-08-17 17:14:16 +0800

    update for reading ma file
    1. add RedshiftNormalMap node
    2. add "addAttr " keyword option
    3. new dso Newtonsoft.Json.dll

---
+++ maReplace/maReplace/maGettings/jsnClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maReplace
+{
+    public sealed class jsnClass
+    {
+        public string index { get; set; }
+
+        public fileClass fiVesClass { get; set; }
+    }
+
+    public class fileClass
+    {
+        public  string sourcefile{get;set;}
+
+        public string destfile{get;set;}
+    }
+}
